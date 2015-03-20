8082f9f376eb6b74d9c1ec8dab49f7d0e570d6e7
jpbazonnais <jean-philippe.bazonnais@ign.fr>
2015-03-20 15:03:25 +0100

    test jenkins pom-1 ...

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -4,23 +4,18 @@ var gutil     = require('gulp-util');
 var normalize = require('gulp-bower-normalize');
 var bower     = require('main-bower-files');
 var debug     = require('gulp-debug');
-var runtask   = require('gulp-bower');
+var uglify    = require('gulp-uglify');
 
 // system file...
 var fs = require('fs');
 
 var targetdir = "./target";
 
-var bDownload = false;
-if(gutil.env.download === true) {
-    bDownload = true;
-}
-
 gulp.task('default', [
     "clean",
-    "download",
-    "normalize",
-    "build"
+    "build:normalize",
+    "build:uglify",
+    "build:dependencies"
 ]);
 
 // nettoyage du repertoire de "build"
@@ -30,27 +25,12 @@ gulp.task("clean", function () {
         .pipe(clean());
 });
 
-// recuperation des dependances via "bower" 
-gulp.task('download', ["clean"], function() {
-    
-     if (bDownload) {
-        return runtask();
-    }
-    
-    gutil.log("SKIP Task : download !");
-});
-
 // normalisation des noms de dependances issu de "bower"
-gulp.task('normalize', ["download"], function() {
+gulp.task('build:normalize', ["clean"], function() {
     
-    // les dependances doivent être placées dans le répertoire : target/build/js/external/
-    // avec la possibilité d'utilisé une version minifiée ou non (cf. --dev)
-    // Il faut normaliser les nons des lib., cad on ne veut pas de *.min.js !
-    // Attention, il est possible qu'il n'y ait pas de lib. à normaliser...
-
     fs.exists('./bower_components', function (exist) {
         if (exist) {
-            gulp.src(bower(), {base: './bower_components'})
+            return gulp.src(bower(), {base: './bower_components'})
 		.pipe(debug({title: ':bower_components'}))
                 .pipe(normalize({bowerJson: './bower.json', flatten:true}))
                 .pipe(gulp.dest('./bower_dependencies/'));
@@ -62,11 +42,26 @@ gulp.task('normalize', ["download"], function() {
     
 });
 
-// copie
-gulp.task('build', ["normalize"], function () {
-    
+gulp.task('build:uglify', ["build:normalize"], function () {
     return gulp.src('./bower_dependencies/js/' + '*.js')
-	.pipe(debug({title: ':build'}))
-        .pipe(gulp.dest(targetdir));
+		.pipe(uglify())
+                .pipe(debug({title: ':uglify'}))
+		.pipe(gulp.dest('./bower_dependencies/js-min/'));
+});
+
+// copie
+gulp.task('build:dependencies', ["build:uglify"], function () {
+	
+    fs.exists('./bower_dependencies', function (exist) {
+        if (exist) {
+		    return gulp.src('./bower_dependencies/js-min/' + '*.js')
+			.pipe(debug({title: ':build'}))
+			.pipe(gulp.dest(targetdir));
+        }
+        else {
+            gutil.log("No bower dependencies !");
+        }
+    });
+    			
 });
 
