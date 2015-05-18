13559b2757cb73f57cd75e3c9d0789144d7035a2
jpbazonnais <jean-philippe.bazonnais@ign.fr>
2015-05-18 16:36:00 +0200

    add .hgignore

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -11,22 +11,44 @@ var fs = require('fs');
 
 var targetdir = "./target";
 
-gulp.task('default', [
-    "clean",
-    "build:normalize",
-    "build:uglify",
-    "build:dependencies"
-]);
+var argv = require('minimist')(process.argv.slice(4));
 
-// nettoyage du repertoire de "build"
+var withBower = false;
+if(gutil.env.withbower === true) {
+    withBower = true;
+}
+
+var withNpm = false;
+if(gutil.env.withnpm === true) {
+    withNpm = true;
+}
+
+if (withBower) {
+    gulp.task('default', [
+        "clean",
+        "build:bower"
+    ]);
+}
+else if (withNpm) {
+    gulp.task('default', [
+        "clean",
+        "build:npm"
+    ]);
+}
+else {
+    gutil.log("No env use with bower or npm  !");
+}
+
 gulp.task("clean", function () {
     return gulp.src(targetdir, {read: false})
         .pipe(debug({title: ':clean'}))
         .pipe(clean());
 });
 
-// normalisation des noms de dependances issu de "bower"
-gulp.task('build:normalize', ["clean"], function() {
+gulp.task('build:bower', ['build:bower:normalize', 'build:bower:uglify', 'build:bower:dependencies']);
+gulp.task('build:npm',   ['build:npm:normalize'  , 'build:npm:uglify'  , 'build:npm:dependencies']);
+
+gulp.task('build:bower:normalize',    ["clean"], function() {
     
     fs.exists('./bower_components', function (exist) {
         if (exist) {
@@ -41,16 +63,13 @@ gulp.task('build:normalize', ["clean"], function() {
     });
     
 });
-
-gulp.task('build:uglify', ["build:normalize"], function () {
+gulp.task('build:bower:uglify',       ["build:bower:normalize"], function () {
     return gulp.src('./bower_dependencies/js/' + '*.js')
 		.pipe(uglify())
                 .pipe(debug({title: ':uglify'}))
 		.pipe(gulp.dest('./bower_dependencies/js-min/'));
 });
-
-// copie
-gulp.task('build:dependencies', ["build:uglify"], function () {
+gulp.task('build:bower:dependencies', ["build:bower:uglify"], function () {
 	
     fs.exists('./bower_dependencies', function (exist) {
         if (exist) {
@@ -65,3 +84,12 @@ gulp.task('build:dependencies', ["build:uglify"], function () {
     			
 });
 
+gulp.task('build:npm:normalize',     ['clean'], function() {
+ 
+});
+gulp.task('build:npm:uglify',        ['build:npm:normalize'], function() {
+    
+});
+gulp.task('build:npm:dependencies',  ['build:npm:uglify'], function() {
+    
+});
